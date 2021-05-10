65a8fcfdbad2f6be15a850ff4d89888918c08f55
Leandro Neves <leandrosoares6@hotmail.com>
2021-05-10 23:51:33 +0000

    refactor: returning uuid string after add a new planet

--- src/main/java/br/com/leandro/swplanets/domain/usecases/AddPlanet.java
+++ src/main/java/br/com/leandro/swplanets/domain/usecases/AddPlanet.java
@@ -1,25 +1,34 @@
 package br.com.leandro.swplanets.domain.usecases;
 
-import java.util.Optional;
-
+import br.com.leandro.swplanets.application.requests.PlanetRequest;
 import br.com.leandro.swplanets.domain.entities.Planet;
 import br.com.leandro.swplanets.domain.exceptions.EntityExistsException;
 import br.com.leandro.swplanets.domain.ports.PlanetRepository;
+import br.com.leandro.swplanets.domain.valueobjects.Name;
 
 public class AddPlanet {
   private PlanetRepository planetRepository;
 
-  public AddPlanet( PlanetRepository planetRepository) {
+  public AddPlanet(PlanetRepository planetRepository) {
     this.planetRepository = planetRepository;
   }
 
-  public void execute(Planet planet) {
-    Optional<Planet> planetFromDd = planetRepository.findById(planet.getId());
+  public String execute(PlanetRequest planetRequest) {
+    Planet planetFromDd = planetRepository
+      .findByName(new Name(planetRequest.getName()));
 
-    if (planetFromDd.isPresent()) {
-      throw new EntityExistsException();
+    if (planetFromDd != null) {
+      throw new EntityExistsException("Entity already exists.");
     }
 
+    Planet planet = new Planet(
+      planetRequest.getName(),
+      planetRequest.getClimate(),
+      planetRequest.getTerrain()
+    );
+
     planetRepository.save(planet);
+
+    return planet.getId();
   }
 }
